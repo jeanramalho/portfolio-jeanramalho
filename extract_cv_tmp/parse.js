5818618b641d9bd3b4e12e9f5deaabc1c617f759
jeanramalho.dev <jeanramalho.dev@gmail.com>
2026-04-17 15:49:10 -0300

    [refact] - Inicia refatoração do portfolio com novo tema

--- extract_cv_tmp/parse.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-const pdf = require('pdf-parse');
-let dataBuffer = fs.readFileSync('/Users/jeanramalho/Documents/Projetos/web/portfolio-jeanramalho/Engenharia de Software/CV/Currículo - Jean Ramalho.pdf');
-pdf(dataBuffer).then(function(data) {
-    console.log(data.text);
-}).catch(err => console.error(err));
